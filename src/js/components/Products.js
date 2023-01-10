8dab8acaccfff117cf9773c494f915b67251d10b
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-01-10 15:06:17 +0000

    Add JS for sub pages

---
+++ src/js/components/Products.js
@@ -0,0 +1,21 @@
+import { templates, select } from '../settings.js';
+import utils from '../utils.js';
+
+class Products {
+  constructor() {
+    const thisProducts = this;
+
+    thisProducts.render();
+  }
+
+  render() {
+    const thisProducts = this;
+
+    const generatedHTML = templates.productsPage(thisProducts.data);
+    thisProducts.element = utils.createDOMFromHTML(generatedHTML);
+    const productsContainer = document.querySelector(select.containerOf.products);
+    productsContainer.appendChild(thisProducts.element);
+  }
+}
+
+export default Products;
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,37 @@
-import { settings } from './settings.js';
+import { settings, select, classNames } from './settings.js';
+import Products from './components/Products.js';
+import Contact from './components/Contact.js';
+import Home from './components/Home.js';
+
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    console.log(thisApp.pages);
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    console.log(thisApp.navLinks);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const clickedElement = this;
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activePage(id);
+      });
+    }
+  },
+
+  activePage: function (id) {
+    for (const page of document.querySelectorAll(select.containerOf.pages)) {
+      console.log(document.querySelectorAll(select.containerOf.pages));
+      console.log(page);
+      page.classList.remove(classNames.active);
+    }
+    document.querySelector('#' + id).classList.add(classNames.active);
+    window.location.hash = `#/${id}`;
+  },
+
   initData: function () {
     const url = settings.db.url + '/' + settings.db.products;
     this.data = {};
@@ -12,9 +44,32 @@ const app = {
       });
   },
 
+  initProducts: function () {
+    const thisApp = this;
+    const productsPage = document.querySelector(select.templateOf.productsPage);
+    thisApp.products = new Products(productsPage);
+  },
+
+  initHome: function () {
+    const thisApp = this;
+    const homePage = document.querySelector(select.templateOf.homePage);
+    thisApp.home = new Home(homePage);
+  },
+
+  initContact: function () {
+    const thisApp = this;
+    const contactPage = document.querySelector(select.templateOf.contactPage);
+    thisApp.contact = new Contact(contactPage);
+  },
+
   init: function () {
     const thisApp = this;
+
     thisApp.initData();
+    thisApp.initProducts();
+    thisApp.initContact();
+    thisApp.initHome();
+    thisApp.initPages();
   },
 };
 
